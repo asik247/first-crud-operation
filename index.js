febfad7b696a2c80f02ddac10d80f81184d3d70f
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-17 00:57:39 +0600

    client site data fetch this server

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ const uri = "mongodb+srv://crudUsers:SrMxSjLmw2Lqz0Qd@cluster0.fdzc9ua.mongodb.n
 app.get('/', (req, res) => {
     res.send('first-crud-operation-server here')
 })
+// users Api
+app.get('/users', (req, res) => {
+    res.send('first-crud-operation-server here')
+})
+// post
+app.post('/users', (req, res) => {
+    const newData = req.body;
+    console.log(newData);
+    res.send(newData)
+})
 // create mongodb client
 const client = new MongoClient(uri, {
     serverApi: {
@@ -27,7 +37,7 @@ async function run() {
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-       
+
     }
 }
 run().catch(console.dir);
