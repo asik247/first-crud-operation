8b719e8c00a65e85722a732ac749c26d99011746
Asikur Rahaman <mdasikur5893@gmail.com>
2026-04-17 00:26:47 +0600

    connected db

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 3000
+// MiddleWare
+app.use(cors())
+app.use(express.json())
+// mongoDB uri
+const uri = "mongodb+srv://crudUsers:SrMxSjLmw2Lqz0Qd@cluster0.fdzc9ua.mongodb.net/?appName=Cluster0";
+// Root Api
+app.get('/', (req, res) => {
+    res.send('first-crud-operation-server here')
+})
+// create mongodb client
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+// async funk
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+       
+    }
+}
+run().catch(console.dir);
+// listinner
+app.listen(port, () => {
+    console.log(`This server running is port ${port}`);
+})
+
+/** crudUsers
+ * SrMxSjLmw2Lqz0Qd
+ * SrMxSjLmw2Lqz0Qd
+ */
